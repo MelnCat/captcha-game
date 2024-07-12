4e7eef632c1bb35a6e6af2a7d913b16e28e3d5d7
MelnCat <melncatuwu@gmail.com>
2024-07-12 14:34:39 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+		<h1>Title</h1>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Captcha.tsx
@@ -0,0 +1,6 @@
+import styles from "./Captcha.module.scss";
+
+export const Captcha = () => {
+	return <article className={styles.captcha}>
+		</article>
+}
